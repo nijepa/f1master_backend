5d1bf70b3ad4e7367933eaafda3151fb065030c5
Nikola Pavicevic <nijepa@gmail.com>
2022-04-01 09:06:31 +0200

    initial commit

---
+++ app/models/user.model.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    username: {
+      type: Sequelize.STRING
+    },
+    firstname: {
+      type: Sequelize.STRING
+    },
+    lastname: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+  });
+  return User;
+};
